8558a1e18589ffe52d00d72295bb1bea9b423256
EEletronicChris <chriscosta.130@gmail.com>
2026-05-13 13:20:18 -0300

    Update MCP2515_Config with alternative baud rate settings
    Commented out alternative configuration for 8 MHz crystal.

--- etapa_3/Testes/pi3_CAN_test/Core/Src/MCP2515_driver.c
+++ etapa_3/Testes/pi3_CAN_test/Core/Src/MCP2515_driver.c
@@ -17,9 +17,14 @@ static void MCP2515_Unselect(MCP2515_t *dev) {
 void MCP2515_Config(MCP2515_t *dev){
 
 	uint8_t resetInst = MCP2515_RESET;
+	// Para um cristal de 16 MHz e saída de 500 kHz
 	uint8_t cnf1 = 0x00;
 	uint8_t cnf2 = 0xD1;
 	uint8_t cnf3 = 0x01;
+	// Para um cristal de 8 MHz e saída de 500 kHz
+	// uint8_t cnf1 = 0x00; 
+    // uint8_t cnf2 = 0x91; 
+    // uint8_t cnf3 = 0x02;
 	uint8_t baudR_msg[]  = {MCP2515_WRITE,MCP2515_CNF3,cnf3,cnf2,cnf1};
 	uint8_t CANITE_msg[] =  {MCP2515_WRITE,MCP2515_CANINTE,0x1C};
 	uint8_t CANCTRL_msg[] = {MCP2515_WRITE,MCP2515_CANCTRL,0b00000100};
