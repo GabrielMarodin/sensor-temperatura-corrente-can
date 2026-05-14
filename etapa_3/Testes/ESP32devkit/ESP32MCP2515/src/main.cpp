6cb419b92937ea2f0504382327efa5c1624bf98a
Gabriel Marodin <gabrielmarodin.glm@gmail.com>
2026-05-14 14:13:00 -0300

    esp32 serial print

--- etapa_3/Testes/ESP32devkit/ESP32MCP2515/src/main.cpp
+++ etapa_3/Testes/ESP32devkit/ESP32MCP2515/src/main.cpp
@@ -1,13 +1,17 @@
 #include <SPI.h>
 #include <mcp2515.h>
+#include <HardwareSerial.h>
  
 struct can_frame canMsg;
 struct MCP2515 mcp2515(5); // CS pin is GPIO 5
  
 #define CAN_ACK_ID 0x037  // CAN ID for acknowledgment
 #define CAN_ID 0x036
+
 void setup()
 {
+  Serial.begin(115200);
+
   SPI.begin();
  
   mcp2515.reset();
@@ -22,12 +26,16 @@ void loop()
   {
     if (canMsg.can_id == CAN_ID)  // Check if the message is from the sender
     {
-      unsigned char* data = canMsg.data; // Combine MSB and LSB
- 
+      int data = (canMsg.data[0] << 8) | canMsg.data[1]; // MSB + LSB
+
+      Serial.print("Data received: ");
+      Serial.print(data);
+
       // Send acknowledgment
       canMsg.can_id  = CAN_ACK_ID;  // Use ACK ID
       canMsg.can_dlc = 0;           // No data needed for ACK
       mcp2515.sendMessage(&canMsg);
+      Serial.println("ACK sent");
     }
   }
 }
